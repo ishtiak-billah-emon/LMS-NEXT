92f837ac3a5d92245fdce3b1380c1b050c703246
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-07-16 14:50:43 +0600

    Feat: Forget or Reset Password page

---
+++ src/app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,9 @@
+import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="min-h-scree flex items-center justify-center p-8">
+      <ForgotPasswordForm />
+    </div>
+  );
+}
