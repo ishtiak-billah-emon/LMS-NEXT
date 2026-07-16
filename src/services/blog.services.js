6b9d999efbb7570c7413f6f9c588133368efee28
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2026-07-16 15:35:26 +0600

    Feat: blog api endpoints

---
+++ src/services/blog.services.js
@@ -0,0 +1,40 @@
+import api from "@/lib/api";
+
+const handleRequest = async (request) => {
+  try {
+    const { data } = await request;
+    return data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const blogService = {
+  getAllBlogs: (params = {}) =>
+    handleRequest(api.get("/blogs", { params })),
+
+  getBlogBySlug: (slug) =>
+    handleRequest(api.get(`/blogs/slug/${slug}`)),
+
+  getBlogById: (blogId) =>
+    handleRequest(api.get(`/blogs/${blogId}`)),
+
+  createBlog: (formData) =>
+    handleRequest(api.post("/blogs/create", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    })),
+
+  updateBlog: (blogId, formData) =>
+    handleRequest(api.patch(`/blogs/${blogId}`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    })),
+
+  deleteBlog: (blogId) =>
+    handleRequest(api.delete(`/blogs/${blogId}`)),
+
+  getTeacherBlogs: (params = {}) =>
+    handleRequest(api.get("/blogs/teacher/my-blogs", { params })),
+
+  toggleBlogFeatured: (blogId) =>
+    handleRequest(api.patch(`/blogs/${blogId}/featured`)),
+};
